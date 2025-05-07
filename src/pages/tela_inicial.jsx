078144ea4a8186c93dddd34cb6f25813d94fb96a
YasSantrb <yasmimsantana119@gmail.com>
2025-05-07 11:32:42 -0300

    feat: adicionando a estilização da tela inicial.

--- src/pages/tela_inicial.jsx
+++ src/pages/tela_inicial.jsx
@@ -1,15 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/tela_inicial.css';
+import "../styles/tela_inicial.css";
 
 function TelaInicial() {
   return (
-    <div class="tela_inicial">
-      <h1>Bem-vindo à página inicial!</h1>
-      <p>Essa é a primeira página do nosso projeto.</p>
-      <Link to="/login">Login</Link>
-      <br />
-      <Link to="/cadastro">Cadastro</Link>
+    <div className="tela_inicial">
+      <img
+        src="src/assets/imagens/Tela inicial (2).png"
+        alt="imagem_de_fundo"
+        className="background_tela"
+      />
+
+      <nav>
+        <div className="menu">
+          <img src="src/assets/imagens/Logo.png" alt="logo_ecotech" />
+          <ul className="lista_inicial">
+            <li>
+              <Link className="link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/cadastro">
+                Criar conta
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="">Sobre nós</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main>
+        <div className="conteudo_inicial">
+          <div className="conteudos_textuais">
+            <h1 className="titulo_inicial">Plataforma de doações</h1>
+            <p className="descricao_inicial">
+              Dê um destino correto aos seus eletrônicos e contribua para um
+              futuro mais <span className="descricao_verde"> verde!</span>
+            </p>
+          </div>
+
+          <div className="botao">
+            <button className="botao_inicial">Faça sua doação!</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
