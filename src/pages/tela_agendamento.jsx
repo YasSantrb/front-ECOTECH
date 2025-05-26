87b94f66b5638cbaa7d1bf6d5d271ce4da1fb05d
rafael <arquivosrafael21@gmail.com>
2025-05-26 11:42:50 -0300

    criando tela de agendamento

--- src/pages/tela_agendamento.jsx
+++ src/pages/tela_agendamento.jsx
@@ -1,10 +1,57 @@
 import "../styles/tela_agendamento.css";
+import point from "../assets/imagens/icon_localizacao.png";
+import fundo from "../assets/imagens/fundo_login_cadastro.jpg";
+import icon_voltar from "../assets/imagens/icon_voltar.png";
 
 
 function TelaAgendamento() {
   return (
-    <div>
-      <p>Oi!</p>
+    <div className="background">
+      <img src={fundo} alt="imagem_de_fundo" className="background" />
+      <div className="top-bar">
+                <a href="/usuario/empresa" className="voltar">
+                  <img src={icon_voltar} alt="voltar" className="icone-voltar" />
+                  VOLTAR
+                </a>
+              </div>
+      <div id="conteiner">
+        <div id="conteiner_01">
+          <div id="conteiner_1">
+            <h1>Agendar Coleta</h1>
+            <p>Preencha os campos abaixo para agendar ou retirar sua coleta!</p>
+          </div>
+          <div id="data">
+            <div id="leftdiv">
+              <p>Data da coleta</p>
+              <input type="date" />
+              <p>Observação sobre a coleta</p>
+              <textarea
+                type="text"
+                placeholder="Insira uma observação sobre o agendamento/retirada da sua coleta!"
+                rows="30"
+              />
+            </div>
+            <div id="rightdiv">
+              <p>Horario da coleta</p>
+              <input type="time" />
+              <p>
+                Quer levar sua doação a um ponto de coleta? Clique no botão
+                abaixo.
+              </p>
+              <div id="buttons">
+                <button>
+                  {" "}
+                  <img src={point} height="80px" />
+                  Pontos de coleta
+                </button>
+                <button>
+                  <h2>Enviar</h2>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/tela_info_doacao.jsx
+++ src/pages/tela_info_doacao.jsx
@@ -1,9 +1,30 @@
 import "../styles/tela_info_doacao.css";
+// import fundo_login_cadastro from "../assets/imagens/fundo_login_cadastro.jpg";
 
 function TelaInfoDoacao() {
   return (
-    <div>
-      <p>Oi!</p>
+    <div className="TelaInfoDoacao">
+      {/* <div className="background_tela">
+        <img
+          src={fundo_login_cadastro}
+          alt="imagem_de_fundo"
+          className="background_tela"
+        />
+      </div> */}
+      <div id="conteiner">
+        <div>
+          <p>foto do item</p>
+          <p>Doador:</p>
+          <p>Especificações:</p>
+        </div>
+        <div>
+          <p>nome do item</p>
+          <p>Informações sobre o produto:</p>
+          <p>Condição:</p>
+          <p>Observações:</p>
+          <button>Agendar Coleta!</button>
+        </div>
+      </div>
     </div>
   );
 }
